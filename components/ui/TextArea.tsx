a7f2a0483065c1aba62e581a727c8c893e435c0c
juejiji <126037604+juejiji@users.noreply.github.com>
2022-04-02 12:32:26 +0000

    database updates

--- components/ui/TextArea.tsx
+++ components/ui/TextArea.tsx
@@ -2,4 +2,4 @@ import * as React from 'react';
 import { cn } from '@/utils/cn';
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttrib
+  extends React.TextareaHTMLAttributes<HTMLTextAreaEl
