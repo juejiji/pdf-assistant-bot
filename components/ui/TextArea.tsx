9e04e3cca9db08f795ad8290cddf07987248521c
juejiji <126037604+juejiji@users.noreply.github.com>
2022-04-19 21:11:59 +0000

    Add change log

--- components/ui/TextArea.tsx
+++ components/ui/TextArea.tsx
@@ -9,4 +9,4 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'flex h-20 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring
+          'flex h-20 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:
