c02aae79662a497a697ffb8f5a78d1477dac434c
juejiji <126037604+juejiji@users.noreply.github.com>
2022-05-06 13:31:09 +0000

    Created CNAME

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -2,4 +2,6 @@
  * Change the index and namespace to your own
  */
 
-const PINECONE_INDEX_NAME = 'langchainjsfundamentals'
+const PINECONE_INDEX_NAME = 'langchainjsfundamentals';
+
+const PINECONE_NAME_SPA
