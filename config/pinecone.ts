f7f12bb3b6ab2e92a86b8e18a6abccc283b41c8d
juejiji <126037604+juejiji@users.noreply.github.com>
2022-05-07 16:06:23 +0000

    Fix examples

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -6,4 +6,4 @@ const PINECONE_INDEX_NAME = 'langchainjsfundamentals';
 
 const PINECONE_NAME_SPACE = 'pdf-test'; //namespace is optional for your vectors
 
-export {
+export { PINECONE_INDEX_NAME, PINECONE_N
