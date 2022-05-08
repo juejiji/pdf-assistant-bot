8728385a9c337507523c5fe323a9f4342c8567b6
juejiji <126037604+juejiji@users.noreply.github.com>
2022-05-08 13:07:52 +0000

    added mongodb

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -6,4 +6,4 @@ const PINECONE_INDEX_NAME = 'langchainjsfundamentals';
 
 const PINECONE_NAME_SPACE = 'pdf-test'; //namespace is optional for your vectors
 
-export { PINECONE_INDEX_NAME, PINECONE_N
+export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
