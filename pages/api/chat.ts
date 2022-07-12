350a22dcd801a778adfce2d5802bac29145c2bb4
juejiji <126037604+juejiji@users.noreply.github.com>
2022-07-12 07:54:04 +0000

    [MRG] merge with lp:openerp-web

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -5,3 +5,31 @@ import { PineconeStore } from 'langchain/vectorstores';
 import { makeChain } from '@/utils/makechain';
 import { pinecone } from '@/utils/pinecone-client';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { question, history } = req.body;
+
+  if (!question) {
+    return res.status(400).json({ message: 'No question in the request' });
+  }
+  // OpenAI recommends replacing newlines with spaces for best results
+  const sanitizedQuestion = question.trim().replaceAll('\n', ' ');
+
+  const index = pinecone.Index(PINECONE_INDEX_NAME);
+
+  /* create vectorstore*/
+  const vectorStore = await PineconeStore.fromExistingIndex(
+    index,
+    new OpenAIEmbeddings({}),
+    'text',
+    PINECONE_NAME_SPACE, //optional
+  );
+
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache, no-transform',
+    Connection: 'keep-alive',
+  });
