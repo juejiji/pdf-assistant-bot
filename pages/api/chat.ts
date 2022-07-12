d9880b1395d7465857dd506b9c5fa60cd41f66a3
juejiji <126037604+juejiji@users.noreply.github.com>
2022-07-12 06:09:03 +0000

    add travis ci support

---
+++ pages/api/chat.ts
@@ -0,0 +1,7 @@
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { OpenAIEmbeddings } from 'langchain/embeddings';
+import { PineconeStore } from 'langchain/vectorstores';
+import { makeChain } from '@/utils/makechain';
+import { pinecone } from '@/utils/pinecone-client';
+import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
