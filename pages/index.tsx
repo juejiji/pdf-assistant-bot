f340ae31969daad713817749a3ecfacaec068cb8
juejiji <126037604+juejiji@users.noreply.github.com>
2022-07-27 06:54:51 +0000

    New translations ja-JP.yml (Catalan)

--- pages/index.tsx
+++ pages/index.tsx
@@ -296,3 +296,29 @@ export default function Home() {
                         <LoadingDots color="#000" />
                       </div>
                     ) : (
+                      // Send icon SVG in input field
+                      <svg
+                        viewBox="0 0 20 20"
+                        className={styles.svgicon}
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                      </svg>
+                    )}
+                  </button>
+                </form>
+              </div>
+            </div>
+            {error && (
+              <div className="border border-red-400 rounded-md p-4">
+                <p className="text-red-500">{error}</p>
+              </div>
+            )}
+          </main>
+        </div>
+        <footer className="m-auto p-4">
+          <a href="https://twitter.com/mayowaoshin">
+            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+          </a>
+        </footer>
+      </Layout>
