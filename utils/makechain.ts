d19e6a45a2608b6f06889ba8356b042edddeb891
juejiji <126037604+juejiji@users.noreply.github.com>
2023-08-15 22:10:07 +0000

    New translations template.pot (Ukrainian)

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,3 +1,4 @@
 import { OpenAIChat } from 'langchain/llms';
 import { LLMChain, ChatVectorDBQAChain, loadQAChain } from 'langchain/chains';
-import { PineconeStore } from 'langchain/vectorstore
+import { PineconeStore } from 'langchain/vectorstores';
+import { Promp
