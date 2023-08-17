7ed283ac7cd55a2cb3745843f55b10fe6598c322
juejiji <126037604+juejiji@users.noreply.github.com>
2023-08-17 10:24:34 +0000

    Added logs

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,4 +1,4 @@
 import { OpenAIChat } from 'langchain/llms';
 import { LLMChain, ChatVectorDBQAChain, loadQAChain } from 'langchain/chains';
 import { PineconeStore } from 'langchain/vectorstores';
-import { Promp
+import { PromptTemplate } from 'langchai
