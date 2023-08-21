9e332acf0846acc2ff585abb64c03506dbb11440
juejiji <126037604+juejiji@users.noreply.github.com>
2023-08-21 06:59:50 +0000

    Updated npm packages

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,4 +2,4 @@ import { OpenAIChat } from 'langchain/llms';
 import { LLMChain, ChatVectorDBQAChain, loadQAChain } from 'langchain/chains';
 import { PineconeStore } from 'langchain/vectorstores';
 import { PromptTemplate } from 'langchain/prompts';
-import { Callb
+import { CallbackManager } from 'langch
