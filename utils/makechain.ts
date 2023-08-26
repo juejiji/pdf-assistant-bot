8b037f206af0563a2a1e53d71c1a2e10a5cdbdc2
juejiji <126037604+juejiji@users.noreply.github.com>
2023-08-26 21:12:03 +0000

    iio: bmg160: IIO_BUFFER and IIO_TRIGGERED_BUFFER are required

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -5,4 +5,6 @@ import { PromptTemplate } from 'langchain/prompts';
 import { CallbackManager } from 'langchain/callbacks';
 
 const CONDENSE_PROMPT =
-  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a
+  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Cha
