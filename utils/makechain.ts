eff2d740ac66623aaedd4b2012f870ae0e567e95
juejiji <126037604+juejiji@users.noreply.github.com>
2023-09-08 21:02:01 +0000

    Move to gitlab

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -14,4 +14,5 @@ Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(
   `You are an AI assistant providing helpful advice. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
-You should only provide hyperlinks that reference the context below. Do NOT make up hyperl
+You should only provide hyperlinks that reference the context below. Do NOT make up hyperlinks.
+If you can't find t
