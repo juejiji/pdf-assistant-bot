224ea162cc6178c6a02be9b7b9b44368dbd163a4
juejiji <126037604+juejiji@users.noreply.github.com>
2023-09-29 07:10:58 +0000

    Update web log stats

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -36,4 +36,4 @@ export const makeChain = (
   const docChain = loadQAChain(
     new OpenAIChat({
       temperature: 0,
-      modelName: 'gpt-4', //change this to
+      modelName: 'gpt-4', //change this to older versions (e.g. gpt-3.5
