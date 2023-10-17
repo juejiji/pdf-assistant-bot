b32be0591f8fe08f5897b3144f50165ab05d01ce
juejiji <126037604+juejiji@users.noreply.github.com>
2023-10-17 06:03:44 +0000

    Update DATABASE.md

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -55,4 +55,6 @@ export const makeChain = (
     combineDocumentsChain: docChain,
     questionGeneratorChain: questionGenerator,
     returnSourceDocuments: true,
-    k: 2, //number of source 
+    k: 2, //number of source documents to return
+  });
+};
